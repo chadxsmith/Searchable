939131d7bba9c5bf06f236e13415561ce8221f9f
Chad Smith <chadxsmith@ymail.com>
2015-06-01 19:51:05 -0400

    latest changes before materialize

--- ajax_calls.js
+++ ajax_calls.js
@@ -47,66 +47,21 @@ function searchQuestionAndAnswer(){
                   else{
 
                       for(i = 0; i < response["items"].length; i++){
-                          // if(!(/<pre>/).test(response["items"][i].body)){
-                          //   continue;
-                          // }
-                          // console.log("Has pre", response.items[i].body);
-                          // var matcher = response.items[i].body.match(/<pre>[^<\/pre>]*<\/pre>/);
-                          // console.dir(matcher);
                           var el = document.createElement("div");
-                          el.innerHTML = response.items[i].body;
-                          for(var x = 0; x < el.childNodes.length; x++){
-                            if(el.childNodes[x].localName === "pre"){
-                              console.dir(el.childNodes[x].innerHTML);
-                               var test = $("#answers").append("<div id='square'><li><a href='http://www.stackoverflow.com/a/" + response["items"][i]["answer_id"] + "'>" + el.childNodes[x].innerHTML + "</a></li></div>").css('display', 'inline');
+                          el.innerHTML = response["items"][i]["body"];
+                          console.log(el.innerHTML)
+                            for(var x = 0; x < el.childNodes.length; x++){
+                              if(el.childNodes[x].localName === "pre"){
+                                // console.log(el.childNodes[x])
+                                $("#answers").append("<div id='square'><li><a href='http://www.stackoverflow.com/a/" + response["items"][i]["answer_id"] + "'>" + el.childNodes[x].innerHTML + "</a></li></div>").css('display', 'inline');
+                           
+                              }
+
 
                             }
-                          }
-                          // var matcher = response["items"][i]["body"].match("/<pre>.*<\/pre>/");
-                          // console.log("foo", matcher);
-                          // var test = $("#answers").append("<div id='square'><li><a href='http://www.stackoverflow.com/a/" + response["items"][i]["answer_id"] + "'>" + matcher + "</a></li></div>").css('display', 'inline');
-
-
-               
-                          // for (x = 0; x < everything.length; x++) {
-                          //         // var pass = everything["items"][x].includes("pre")
-                          //         // console.log(everything)
-                          // }
-                              // for (x = 0; x < code.length; x++){
-                              //      // var apple = code[x].includes("pre")
-                              //      console.log(code)
-                              //  //      if (code[x].includes("pre")){ 
-                              //  //      // console.log(code[x])
-
-                              //  // }
-
-                              // }
-
-                              // var test = $("#answers").append("<div id='square'><li><a href='http://www.stackoverflow.com/a/" + response["items"][i]["answer_id"] + "'>" + response["items"][i]["body"] + "</a></li></div>").css('display', 'inline');
-                          
-                          }
-
-
-                          //Second attempt/ Reasoning: Display each individual "pre"
-                          // var testAgain = $(test).find("pre")
-                          // // console.log(testAgain)
-                          // for (t = 0; t < testAgain.length; t++){
-                          // console.log(testAgain)
-                          // $("#testanswers").append("<div id='square'><li>" + testAgain[t].innerHTML + "</li><div>").css('display', 'inline') 
-                          // }
-                      
-           
-
-
-
-                          //First attempt/ Reasoning: Could not figure out how to get the "pre" objects only 
-                          // var test = $("#answers").append("<div id='square'><li><a href='http://www.stackoverflow.com/a/" + response["items"][i]["answer_id"] + "'>" + (response["items"][i]["body"]).find("pre").prevObject[2].innerHTML + "</a></li></div>").css('display', 'inline');       
-                          // var again = $(response["items"][i]["body"]).find("pre").prevObject
-                          // console.log(again.find("pre").prevObject)
-     
 
                           
-              
+                        }              
       
                  }
                 
